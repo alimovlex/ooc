79588360fd9655e659d8da8be0b9d7efd37bf030
alimovlex <alimovlex@hotmail.com>
2020-05-14 19:35:52 +0300

    The project has been moved to the CMake building system.

--- 04/points.c
+++ 04/points.c
@@ -1,21 +1,14 @@
 #include "Point.h"
 #include "new.h"
+#include "Circle.h"
 
 int main (int argc, char ** argv)
-{	void * p;
-
-	while (* ++ argv)
-	{	switch (** argv) {
-		case 'p':
-			p = new(Point, 1, 2);
-			break;
-		default:
-			continue;
-		}
+{
+        void * p;
+        p = new(Point, 1, 2);
 		draw(p);
 		move(p, 10, 20);
 		draw(p);
 		delete(p);
-	}
 	return 0;
 }

d1f1174938feb1ef2af7bd6c6b30c392df05fd85
alimovlex <alimovlex@hotmail.com>
2020-05-14 19:54:19 +0300

    The project has been build according to the empty requirements.

--- 04/main.c
+++ 04/main.c
@@ -2,23 +2,14 @@
 #include "new.h"
 
 int main (int argc, char ** argv)
-{	void * p;
-
-	while (* ++ argv)
-	{	switch (** argv) {
-		case 'c':
-			p = new(Circle, 1, 2, 3);
-			break;
-		case 'p':
-			p = new(Point, 1, 2);
-			break;
-		default:
-			continue;
-		}
+{	void *p;
+    void *s;
+		p = new(Circle, 1, 2, 3);
+		s = new(Point, 1, 2);
 		draw(p);
-		move(p, 10, 20);
-		draw(p);
+		move(s, 10, 20);
+		draw(s);
 		delete(p);
-	}
+		delete(s);
 	return 0;
 }
